7e6c5787c6d3719214aae82133cf8cf823968054
root <root@10-172-31-12.cloud.opsource.net>
2018-05-10 17:40:31 -0400

    add second echo

--- print-hello.sh
+++ print-hello.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "1st: Hello $NAME, script being run from `hostname`"
 NAME=$1
-echo "Hello $NAME, script being run from `hostname`"
+echo "2nd: Hello $NAME, script being run from `hostname`"
 
