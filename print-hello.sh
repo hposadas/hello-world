cc2be5a2640721b8e868ce483356ede0b4aac5ee
root <harold.posadas@itaas.dimensiondata.com>
2018-05-10 17:57:12 -0400

    update and test internal/external variables

--- print-hello.sh
+++ print-hello.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+
+NAME_IN_SCRIPT=$1
+
+if [ -z "$NAME" ] ; then
+	echo "NAME variable empty"
+fi
+
 echo "1st: Hello $NAME, script being run from `hostname`"
-NAME=$1
-echo "2nd: Hello $NAME, script being run from `hostname`"
+echo "2nd: Hello $NAME_IN_SCRIPT, script being run from `hostname`"
 
