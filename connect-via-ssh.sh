527c70e166488e7ebb825470511912fbfe83cf83
harold.posadas <harold.posadas@itaas.dimensiondata.com>
2018-05-23 10:35:29 -0700

    add connect-via-ssh.sh

---
+++ connect-via-ssh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+NAME_IN_SCRIPT=$1
+
+if [ -z "$NAME" ] ; then
+	echo "NAME variable empty"
+fi
+
+echo "1st: Hello $NAME, script being run from `hostname`"
+echo "2nd: Hello $NAME_IN_SCRIPT, script being run from `hostname` with IP `hostname -i`"
+
+ssh $NAME_IN_SCRIPT@10.146.0.21 'cat /etc/redhat-release'
